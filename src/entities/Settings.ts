f69e8def65ba1b8ee902df69fffc055b2f4b40e6
Ian Torquato <iantorquato2@gmail.com>
2021-04-21 01:07:36 -0300

    Entity -> Settrings + add uuid

---
+++ src/entities/Settings.ts
@@ -0,0 +1,26 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from 'typeorm'
+import { v4 as uuid } from 'uuid'
+
+@Entity('settings')
+export class Settings {
+  constructor() {
+    if (this.id) {
+      this.id = uuid()
+    }
+  }
+
+  @PrimaryColumn({ name: 'settings_id' })
+  id: string
+
+  @Column()
+  username: string
+
+  @Column()
+  chat: string
+
+  @CreateDateColumn()
+  created_at: Date
+
+  @UpdateDateColumn()
+  updated_at: Date
+}
