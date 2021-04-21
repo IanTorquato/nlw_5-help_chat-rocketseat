b95bbf9128af545b74c1e71983f12df68bf33993
Ian Torquato <iantorquato2@gmail.com>
2021-04-21 01:05:22 -0300

    Database sqlite3 + typeorm + Migration -> settings

---
+++ src/database/migrations/1618976099051-CreateSettings.ts
@@ -0,0 +1,39 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class CreateSettings1618976099051 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(new Table({
+      name: 'settings',
+      columns: [
+        {
+          name: 'settings_id',
+          type: 'uuid',
+          isPrimary: true
+        },
+        {
+          name: 'username',
+          type: 'varchar',
+        },
+        {
+          name: 'chat',
+          type: 'boolean',
+          default: true
+        },
+        {
+          name: 'created_at',
+          type: 'timestamp',
+          default: "now()"
+        },
+        {
+          name: 'updated_at',
+          type: 'timestamp',
+          default: "now()"
+        },
+      ]
+    }))
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('settings')
+  }
+}
